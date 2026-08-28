3fe736a81c329216897d313124dfbdee3b4e562c
Yong <will19ie95@gmail.com>
2026-08-27 22:17:58 -0400

    (feat) add bufferline, statusline, logo

---
+++ nvim/lua/plugins/ui.lua
@@ -0,0 +1,77 @@
+return {
+  -- messages, cmdline and the popupmenu
+  {
+    "snacks.nvim",
+    opts = {
+      scroll = { enabled = false },
+    },
+    keys = {},
+  },
+
+  -- buffer line
+  {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    keys = {
+      { "<Tab>", "<Cmd>BufferLineCycleNext<CR>", desc = "Next tab" },
+      { "<S-Tab>", "<Cmd>BufferLineCyclePrev<CR>", desc = "Prev tab" },
+    },
+    opts = {
+      options = {
+        mode = "tabs",
+        -- separator_style = "slant",
+        show_buffer_close_icons = false,
+        show_close_icon = false,
+      },
+    },
+  },
+
+  -- statusline
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = function(_, opts)
+      local LazyVim = require("lazyvim.util")
+      opts.sections.lualine_c[4] = {
+        LazyVim.lualine.pretty_path({
+          length = 0,
+          relative = "cwd",
+          modified_hl = "MatchParen",
+          directory_hl = "",
+          filename_hl = "Bold",
+          modified_sign = "",
+          readonly_icon = " 󰌾 ",
+        }),
+      }
+    end,
+  },
+
+  {
+    "folke/zen-mode.nvim",
+    cmd = "ZenMode",
+    opts = {
+      plugins = {
+        gitsigns = true,
+        tmux = true,
+      },
+    },
+    keys = { { "<leader>z", "<cmd>ZenMode<cr>", desc = "Zen Mode" } },
+  },
+
+  {
+    "folke/snacks.nvim",
+    opts = {
+      dashboard = {
+        preset = {
+          header = [[
+██╗  ██╗ █████╗ ███╗   ███╗██╗███╗   ██╗ █████╗ ██████╗ ██╗
+██║ ██╔╝██╔══██╗████╗ ████║██║████╗  ██║██╔══██╗██╔══██╗██║
+█████╔╝ ███████║██╔████╔██║██║██╔██╗ ██║███████║██████╔╝██║
+██╔═██╗ ██╔══██║██║╚██╔╝██║██║██║╚██╗██║██╔══██║██╔══██╗██║
+██║  ██╗██║  ██║██║ ╚═╝ ██║██║██║ ╚████║██║  ██║██║  ██║██║
+╚═╝  ╚═╝╚═╝  ╚═╝╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝╚═╝  ╚═╝╚═╝
+   ]],
+        },
+      },
+    },
+  },
+}
